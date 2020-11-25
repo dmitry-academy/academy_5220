cb9ca33b036ed537e5a6bf5d23b05080608caa88
dmitrysc <dmitrysc@t480-SchepinD.playtika.local>
2020-11-25 17:33:07 +0300

    lesson 10

---
+++ academy5220/src/by/academy/lesson10/innerclasses/City.java
@@ -0,0 +1,15 @@
+package by.academy.lesson10.innerclasses;
+public class City {
+    public static class District {
+        public void go() {
+            System.out.println("Go to the District.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Town4.Street street = new Town4.Street();
+        street.go();
+        District district = new District();
+        district.go();
+    }
+}
